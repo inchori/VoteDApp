cb3c6552d00f13eff1f30df35a584a354f3f7a8c
sic616 <sic61695@gmail.com>
2019-05-21 20:09:10 +0900

    Second commit

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,4 +1,4 @@
-pragma solidity ^"0.4.2";
+pragma solidity ^"0.5.0";
 
 contract Election {
     // Model a Candidate
@@ -19,7 +19,7 @@ contract Election {
         uint indexed _candidateId
     );
 
-    function Election () public {
+    constructor() public {
         addCandidate("Candidate 1");
         addCandidate("Candidate 2");   
     }
@@ -38,10 +38,10 @@ contract Election {
         candidates[_candidateId].voteCount++;
 
         // trigger voted event
-        votedEvent(_candidateId);
+        emit votedEvent(_candidateId);
     }
 
-    function addCandidate (string _name) private {
+    function addCandidate (string memory _name) private {
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
